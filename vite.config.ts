a13ef8e2af1fa54a6e64361ae8acc06630daf9f5
VV01T3K <wojteks.access@gmail.com>
2026-03-25 01:09:03 +0000

    feat: modernize the tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,33 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest/config" />
+
+import { defineConfig } from 'vitest/config'
 import { varlockVitePlugin } from '@varlock/vite-integration'
 import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
+import babel from "@rolldown/plugin-babel";
+import { reactCompilerPreset } from "@vitejs/plugin-react";
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  test: {
+    environment: 'happy-dom',
+  },
   plugins: [
     varlockVitePlugin({ ssrInjectMode: 'auto-load' }),
     devtools(),
     nitro({ preset: 'bun', rollupConfig: { external: [/^@sentry\//] } }),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
-    viteReact({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
+    viteReact(),
+    babel({
+      presets: [reactCompilerPreset()],
     }),
   ],
 })
